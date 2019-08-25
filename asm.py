465ab9a6f1ce6707bbfaf30541ecb85a8013f14c
dd0 <de298@cam.ac.uk>
2019-08-25 23:14:47 +0200

    Initial implementation of CPU and assembler.

---
+++ asm.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+from cpu import BotCPU
+import sys
+import struct
+
+
+def asm_instruction(line):
+    instr = line.split()[0]
+    params = ''.join(line.split()[1:]).split(',')
+
+    cond = None
+    if '.' in instr:
+        instr, cond = instr.split('.')
+    
+    else:
+        opcode, fmt = None, None
+
+        if instr == 'B':
+            opcode, fmt = (25, 'C') if cond else (26, 'O')
+        else:
+            candidates = [(i, x[1]) for i, x in enumerate(BotCPU.OPERATIONS) if x[0] == instr]
+            if len(candidates) == 0:
+                raise "Unknown instruction " + instr
+            elif len(candidates) == 1:
+                opcode, fmt = candidates[0]
+            elif not params[-1].isnumeric():
+                opcode, fmt = candidates[0][0], 'R'
+            else:
+                opcode, fmt = candidates[0][0] + 1, ('I' if instr in ['CMP', 'MOV'] else 'D')
+
+    def encode(lens, vals):
+        res = 0
+        for l, v in zip(lens, vals):
+            res = (res << l) | (v & (2**l - 1))
+        return res
+
+    print(line)
+    print(instr, opcode, params, fmt)
+
+    regs = {'R0': 0, 'R1': 1, 'R2': 2, 'R3': 3, 'R4': 4, 'R5': 5, 'SP': 6, 'PC': 7}
+    params = [int(regs.get(p, p)) for p in params]
+
+    print(instr, opcode, params, fmt)
+    
+    if fmt == 'R':
+        while len(params) < 3: params.append(0)
+        return encode([5, 3, 3, 3, 2], [opcode, params[0], params[1], params[2], 0])
+    elif fmt == 'D':
+        return encode([5, 3, 3, 5], [opcode, params[0], params[1], params[2]])
+    elif fmt == 'I':
+        return encode([5, 3, 8], [opcode, params[0], params[1]])
+    elif fmt == 'C':
+        cond_map = {x[0]: i for i, x in enumerate(BotCPU.COND)}
+        cond_code = cond_map[cond]
+        return encode([5, 4, 7], [opcode, cond_code, params[0]])
+    elif fmt == 'O':
+        return encode([5, 11], [opcode, params[0]])
+        
+
+def main():
+    #code = b''
+    code = []
+    for line in sys.stdin:
+        #code += struct.pack('>H', asm_instruction(line.strip()))
+        code.append(asm_instruction(line.strip()))
+    print(code)
+    
+if __name__ == '__main__':
+    main()
---
+++ cpu.py
@@ -0,0 +1,166 @@
+#! /usr/bin/env python
+
+class BotCPU:
+    # Configuration
+    MEM_SIZE = 2**16
+    STACK_SIZE = 2**12
+
+    # Register names
+    SP = 6
+    PC = 7
+    
+    def __init__(self):
+        self.memory = [0 for _ in range(BotCPU.MEM_SIZE)]
+        self.stack = [0 for _ in range(BotCPU.STACK_SIZE)]
+        self.regs = [0 for _ in range(8)]
+        self.flags = False, False, False, False
+
+    def load_code(self, data):
+        for i, d in enumerate(data):
+            self.memory[i] = d
+        # Start execution from first instruction
+        self.regs[BotCPU.PC] = 0
+
+
+    def pc(self):
+        return self.regs[BotCPU.PC]
+
+
+    # Pairs of (name, encoding), where encoding is one of:
+    #   R: Rdest, R1, R2
+    #   D: Rdest, R, imm
+    #   I: Rdest, imm
+    #   C: cond, imm
+    #   O: imm only
+    OPERATIONS = [
+        ('MOV', 'R'),
+        ('MOV', 'I'),
+        ('ADD', 'R'),
+        ('ADD', 'D'),
+        ('SUB', 'R'),
+        ('SUB', 'D'),
+        ('LSH', 'R'),
+        ('LSH', 'D'),
+        ('RSH', 'R'),
+        ('RSH', 'D'),
+        ('AND', 'R'),
+        ('AND', 'D'),
+        ('OR', 'R'),
+        ('OR', 'D'),
+        ('XOR', 'R'),
+        ('XOR', 'D'),
+        ('LOAD', 'R'),
+        ('LOAD', 'D'),
+        ('STORE', 'R'),
+        ('STORE', 'D'),
+        ('PUSH', 'R'),
+        ('PUSH', 'O'),
+        ('POP', 'R'),
+        ('CMP', 'R'),
+        ('CMP', 'I'),
+        ('B', 'C'),
+        ('B', 'O'),
+        ('SYSCALL', 'I')
+    ]
+
+    ARITH = {
+        'ADD': lambda x, y: x + y,
+        'SUB': lambda x, y: x - y,
+        'LSH': lambda x, y: x << y,
+        'RSH': lambda x, y: x >> y,
+        'AND': lambda x, y: x & y,
+        'OR': lambda x, y: x | y,
+        'XOR': lambda x, y: x ^ y
+    }
+
+    COND = [
+        ('EQ', lambda n, z, c, v: z),
+        ('NE', lambda n, z, c, v: not z),
+        ('CS', lambda n, z, c, v: c),
+        ('CC', lambda n, z, c, v: not c),
+        ('MI', lambda n, z, c, v: n),
+        ('PL', lambda n, z, c, v: not n),
+        ('VS', lambda n, z, c, v: v),
+        ('VC', lambda n, z, c, v: not v),
+        ('HI', lambda n, z, c, v: c and not z),
+        ('LS', lambda n, z, c, v: not c or z),
+        ('GE', lambda n, z, c, v: n == v),
+        ('LT', lambda n, z, c, v: n != v),
+        ('GT', lambda n, z, c, v: not z and (n == v)),
+        ('LE', lambda n, z, c, v: z or (n != v)),
+        ('AL', lambda n, z, c, v: True),
+        ('NV', lambda n, z, c, v: False),
+    ]
+    
+    def decode(instr):
+        def field(off, L):
+            return (instr >> off) & (2**L - 1)
+        
+        opcode = field(11, 5)
+        R, imm, cond = None, None, None
+
+        fmt = BotCPU.OPERATIONS[opcode][1]
+
+        if fmt == 'R':
+            R, imm = [field(8, 3), field(5, 3), field(2, 3)], None
+        elif fmt == 'D':
+            R, imm = [field(8, 3), field(5, 3)], field(0, 5)
+        elif fmt == 'I':
+            R, imm = [field(8, 3)], field(0, 8)
+        elif fmt == 'C':
+            R, imm, cond = [field(4, 3)], field(0, 7), field(7, 4)
+        elif fmt == 'O':
+            R, imm = [], field(0, 11)
+
+        return opcode, R, imm, cond
+
+
+    def set_reg(self, r, x):
+        if r < 0 or r > 7:
+            raise "Register out of range"
+        self.regs[r] = x & 0xFFFF
+
+    
+    def step(self):
+        # Decode
+        instr = self.memory[self.pc()]
+        opcode, R, imm, cond = BotCPU.decode(instr)
+
+        name, fmt = BotCPU.OPERATIONS[opcode]
+        params = None
+
+        if fmt == 'R':
+            params = self.regs[R[1]], self.regs[R[2]]
+        elif fmt == 'D':
+            params = self.regs[R[1]], imm
+
+        # Execute
+        if name in BotCPU.ARITH:
+            self.set_reg(R[0], BotCPU.ARITH[name](*params))
+        elif name == 'MOV':
+            self.set_reg(R[0], self.regs[R[1]] if fmt == 'R' else imm)
+        elif name == 'LOAD':
+            self.set_reg(R[0], self.get_mem(params[0] + params[1]))
+        elif name == 'STORE':
+            self.set_mem(params[0] + params[1], self.regs[R[0]])
+        elif name == 'PUSH':
+            self.push(self.regs[R[0]] if fmt == 'R' else imm)
+        elif name == 'POP':
+            self.set_reg(R[0], self.pop())
+        elif name == 'CMP':
+            # flags are NZCV
+            res = params[0] - params[1]
+            self.flags = res < 0, res == 0, res > 0xFFFF, res > 0x7FFF
+        elif name == 'B':
+            should_jump = BotCPU.COND[cond][1](*self.flags) if fmt == 'C' else True
+            if should_jump:
+                # set two bytes before jump target since we will increment PC (note: cells are 2-byte)
+                self.set_reg(BotCPU.PC, imm - 1)
+        elif name == 'SYSCALL':
+            # TODO
+            print(self.regs[R[0]])
+        else:
+            raise "Unknown instruction " + name
+
+        # next instruction
+        self.set_reg(BotCPU.PC, self.pc() + 1)
